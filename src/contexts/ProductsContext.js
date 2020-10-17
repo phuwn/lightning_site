a4828c5f7b1fa19630a844d8858d2f2d3f136ed9
Phuong <phuwn29@gmail.com>
2020-10-17 07:49:07 +0700

    Feature - get items from api

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -1,16 +1,24 @@
-import React, { createContext, useState } from 'react';
-import { dummyProducts } from '../services/dummy';
-export const ProductsContext = createContext()
+import React, { createContext, useState } from "react";
+export const ProductsContext = createContext();
+// process.env.REACT_APP_API_ENDPOINT.concat("/product")
+const ProductsContextProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  fetch("http://localhost:8080/product")
+    .then((response) => response.json())
+    .then(({ code, data, error }) => {
+      console.log(data);
+      if (code !== 200) {
+        console.log(error);
+        return;
+      }
+      setProducts(data);
+    });
 
-const ProductsContextProvider = ({children}) => {
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
 
-    const [products] = useState(dummyProducts);
-
-    return ( 
-        <ProductsContext.Provider value={{products}} >
-            { children }
-        </ProductsContext.Provider>
-     );
-}
- 
 export default ProductsContextProvider;
